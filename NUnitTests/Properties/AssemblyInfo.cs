ba0f43ba71258f1105e428dc92cc657999f3e178
psilo <gu_falcon@hotmail.com>
2018-01-20 00:01:04 +0100

    Major restructuring

--- NUnitTests/Properties/AssemblyInfo.cs
+++ NUnitTests/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
-[assembly: AssemblyProduct("Test")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("NUnitTests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Unterrainer Informatik OG")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("26523d3f-1cf3-44cc-9855-c0daf3fe6913")]
+[assembly: Guid("5b504126-f927-4fb6-b560-d10699b09ffa")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.1.2")]
-[assembly: AssemblyFileVersion("3.4.1.2")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
